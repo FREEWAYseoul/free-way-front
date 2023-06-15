d827cea078a677646771f51524b33338e14d4182
jlee0505 <hj960322@gmail.com>
2023-06-16 08:38:58 +0900

    fix: 띄어쓰기 오류

--- src/hooks/useMic.tsx
+++ src/hooks/useMic.tsx
@@ -11,7 +11,7 @@ const useMic = () => {
 
   const { listen, stop, listening } = useSpeechRecognition({
     onResult: (result: string) => {
-      setKeywords(result.replace(/\s/, ''));
+      setKeywords(result.replace(/\s/g, ''));
     },
     onEnd: () => {
       const selectedStation = selectStationByKeywords(keywords);
