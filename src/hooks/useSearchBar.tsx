1dfb5f90635325e42b6fc2519dde82515bef4111
jlee0505 <hj960322@gmail.com>
2023-06-15 14:50:24 +0900

    fix: 역삼역 검색 안되는 에러 수정

--- src/hooks/useSearchBar.tsx
+++ src/hooks/useSearchBar.tsx
@@ -35,7 +35,7 @@ const useSearchBar = () => {
   // 실행되는 곳: Search 페이지 useEffect시
   const getFilteredStations = useCallback(
     (keywords: string) => {
-      const character = keywords?.replace('역', '').trim();
+      const character = keywords?.replace(/역$/, '').trim();
       const wordsStartingWithKeywords = autocomplete.searchPrefix(character);
       const filteredStations = data.filter((station: Station) =>
         wordsStartingWithKeywords.includes(station.stationName)
