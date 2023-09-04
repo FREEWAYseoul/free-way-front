f28d984ce7ac345425d1802fed6aa548d0b1c433
bbung <tjd123123@naver.com>
2023-09-04 15:54:09 +0900

    style: search-bar css fix
    - flex를 grid로 변경
    - mic 버튼이 밀리는 이슈

--- src/components/domain/Search/SearchBar.tsx
+++ src/components/domain/Search/SearchBar.tsx
@@ -25,30 +25,25 @@ const SearchBar = ({ placeholder, listeningMessage, handleClick, isListening }: 
   return (
     <>
       <StyledSearchBarWrapper>
-        <StyledLeftSection>
-          <Button handleClick={handleGoBack}>
-            <ChevronIcon style={{ width: '24px', height: '24px' }} />
-          </Button>
-          <form
-            onSubmit={handleSubmit}
-            style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flex: 1 }}
-          >
-            <StyledSearchBarInput
-              id='search-bar'
-              value={keywords || ''}
-              type='text'
-              placeholder={isListening ? listeningMessage : placeholder}
-              onChange={handleTyping}
-              ref={inputRef}
-            />
-          </form>
-        </StyledLeftSection>
-
-        <StyledRightSection>
-          <Button handleClick={handleClick}>
-            <MicIcon />
-          </Button>
-        </StyledRightSection>
+        <Button handleClick={handleGoBack}>
+          <ChevronIcon style={{ width: '24px', height: '24px' }} />
+        </Button>
+        <form
+          onSubmit={handleSubmit}
+          style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+        >
+          <StyledSearchBarInput
+            id='search-bar'
+            value={keywords || ''}
+            type='text'
+            placeholder={isListening ? listeningMessage : placeholder}
+            onChange={handleTyping}
+            ref={inputRef}
+          />
+        </form>
+        <Button handleClick={handleClick}>
+          <MicIcon />
+        </Button>
       </StyledSearchBarWrapper>
     </>
   );
@@ -62,19 +57,14 @@ const StyledSearchBarWrapper = styled.div`
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
 
   display: grid;
-  grid-template-columns: 1fr 75px;
+  grid-template-columns: 36px 1fr 75px;
   background-color: #ffffff;
+
+  padding-left: 13px;
 
   font-style: normal;
   font-weight: 600;
   font-size: 1.125rem;
-`;
-
-const StyledLeftSection = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  padding-left: 13px;
 `;
 
 const StyledSearchBarInput = styled.input`
@@ -94,10 +84,4 @@ const StyledSearchBarInput = styled.input`
     font-weight: 600;
     line-height: normal;
   }
-`;
-
-const StyledRightSection = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
 `;
